5b7eddfa4823e890c5a1cff9f410314b7236d755
Vitor Abreu <vitorvaf@gmail.com>
2023-11-24 19:00:56 -0300

    adicionando contratos e organizando as dependencias e contratos entre os serviços.

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -2,13 +2,19 @@ using MassTransit;
 using Polly;
 using Polly.Extensions.Http;
 using SearchService;
-
+using AutoMapper;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetRetryPolicy());
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 
 builder.Services.AddMassTransit(x =>
 {
+    x.AddActivitiesFromNamespaceContaining<AuctionCreatedConsumer>();
+
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("search", false));
+
     x.UsingRabbitMq((context, cfg) =>
     {
         cfg.Host("localhost", "/", h => {
