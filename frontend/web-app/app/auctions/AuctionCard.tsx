2b2455e690ee95aa4a3a6dfb5f84412c0124a1ee
Vitor Abreu <vitorvaf@gmail.com>
2024-02-22 11:39:06 -0300

    fix(layout): Corrigindo laytout do componente AuctionCard

--- frontend/web-app/app/auctions/AuctionCard.tsx
+++ frontend/web-app/app/auctions/AuctionCard.tsx
@@ -6,20 +6,22 @@ import { Auction } from '@/types';
 type Props = {
     auction: Auction
 };
+export default function AuctionCard({ auction }: Props) {
+  return (
+      <a href='#' className='group'>
+          <div className='w-full bg-gray-200 aspect-w-16 aspect-h-10 rounded-lg overflow-hidden'>
+              <div>
+                  <CarImage imageUrl={auction.imageUrl} />
+                  <div className='absolute bottom-2 left-2'>
+                      <CountdownTimer auctionEnd={auction.auctionEnd} />
+                  </div>
+              </div>
+          </div>
+          <div className='flex justify-between items-center mt-4'>
+              <h3 className='text-gray-700'>{auction.make} {auction.model}</h3>
+              <p className='font-semibold text-sm'>{auction.year}</p>
+          </div>
 
-export default function AuctionCard({auction}: Props) {
-  return(
-    <a href='#' className='group'>
-        <div className='w-full bg-gray-200 aspect-w-16 aspect-h-10 rounded-lg overflow-hidden'>
-           <CarImage imageUrl={auction.imageUrl} />
-            <div className='absolute bottom-2 left-2'>
-              <CountdownTimer auctionEnd={auction.auctionEnd} />
-            </div>            
-        </div>
-        <div className='flex justify-between items-center mt-4'>
-          <h3 className='text-gray-700'> {auction.make} {auction.model}</h3>
-          <p className='font-semibold text-sm'>{auction.year}</p>
-        </div>        
-    </a>
+      </a>
   )
-}
+}       
