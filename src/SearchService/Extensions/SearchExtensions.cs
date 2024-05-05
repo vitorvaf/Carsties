57f7f8cf76037a5c8864c6921bab021aa5551d49
Vitor Abreu <vitorvaf@gmail.com>
2024-05-05 17:01:31 -0300

    ordenando busca e configuração de identity.

--- src/SearchService/Extensions/SearchExtensions.cs
+++ src/SearchService/Extensions/SearchExtensions.cs
@@ -19,7 +19,8 @@ public static class SearchExtensions
 
             query = searchParams.OrderBy switch
             {
-                "make" => query.Sort(x => x.Ascending(a => a.Make)),
+                "make" => query.Sort(x => x.Ascending(a => a.Make))
+                    .Sort(x => x.Ascending(a => a.Model)),
                 "new" => query.Sort(x => x.Ascending(a => a.CreatedAt)),
                 "year" => query.Sort(x => x.Ascending(a => a.Year)),
                 _ => query.Sort(x => x.Ascending(a => a.AuctionEnd))
